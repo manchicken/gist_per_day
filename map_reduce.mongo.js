8fe2d5378a4a2ce39827c1530165603e0aa7b429
Michael D. Stemle, Jr <themanchicken@gmail.com>
2013-08-25 06:19:00 +0000

    Got the script working in non-interactive mode!

--- map_reduce.mongo.js
+++ map_reduce.mongo.js
@@ -1,1 +1,17 @@
-b.foo.mapReduce( function () { emit (this['First Name'].toLowerCase().split('')[0], 1); }, function (key,value) { return Array.sum(value); }, { out : " foo_out" })
+db.foo.find().forEach(printjson);
+
+//print(db.foo.find());
+
+db.foo.mapReduce(
+  function () {
+    emit (this["First Name"].toLowerCase().split('')[0], 1);
+  },
+  function (key,value) {
+    return Array.sum(value);
+  },
+  {
+    out : "foo_out"
+  }
+);
+
+db.foo_out.find().forEach(printjson);
