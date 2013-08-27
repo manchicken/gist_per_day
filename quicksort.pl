f9478d09154f8fb966c8571ca5d1949faee0c728
Michael D. Stemle, Jr <themanchicken@gmail.com>
2013-08-27 19:40:00 +0000

    Finished the algorithm...

--- quicksort.pl
+++ quicksort.pl
@@ -1,50 +1,41 @@
 #!/usr/bin/env perl
 use strict;use warnings;
 
-use Test::More;
+use Test::More tests => 5;
+use Data::Dumper;
 
 sub quicksort {
-  my ($list) = @_;
+  my (@list) = @_;
   
-  my $len = scalar(@{$list});
+  my $len = scalar(@list);
   
   # 1 or fewer elements in the list constitute a sorted list :)
-  return $list unless ($len > 1);
+  return @list unless ($len > 1);
   
   # Picking the middle as a pivot
   my $pivot_pos = int($len / 2);
-  my $hold = undef;
   
-  my $pivot_val = $list->[$pivot_pos];
+  my $pivot_val = $list[$pivot_pos];
   
-  my $righty = $len-1;
-  my $lefty = 0;
+  my @greater = ();
+  my @lesser = ();
+  my @equal = ();
   
-  while ($lefty <= $righty) {
-    $lefty += 1 while ($list->[$lefty] < $pivot_val);
-    $righty -= 1 while ($list->[$righty] > $pivot_val);
-    
-    if ($list->[$lefty] <= $list->[$righty]) {
-      $hold = $list->[$lefty];
-      $list->[$lefty] = $list->[$righty];
-      $list->[$righty] = $hold;
-      undef $hold;
-      $lefty += 1;
-      $righty -= 1;
-    }
+  my $lid = 0;
+  for my $item (@list) {
+    push(@lesser, $item) if ($item < $pivot_val);
+    push(@equal, $item) if ($item == $pivot_val);
+    push(@greater, $item) if ($item > $pivot_val);
   }
-  quicksort(@{$list}[0..$pivot_pos-1]);
-  quicksort(@{$list}[$pivot_pos..$len]);
   
-  return $list;
+  return (quicksort(@lesser), @equal, quicksort(@greater));
 }
 
 my @list = qw{1 6 3 9 8 6 2 5 7};
-quicksort(\@list);
+my @output = quicksort(@list);
 
-use Data::Dumper;
-print Dumper(\@list);
-
-
-
-
+is(scalar(@list), scalar(@output), 'Verify we did not change the size of the lists...');
+is($output[2], 3, 'Verify that item 2 is 3...');
+is($output[-1], 9, 'Verify that the last item is a 9...');
+is($output[4], 6, 'Verify that item 4 is a 6...');
+is($list[-1], 7, 'Verify that the last item of the input was a 7...');
