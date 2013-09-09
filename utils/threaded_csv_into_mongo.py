37e8460c026a47d1681f19dbd3cb3741b9606a34
Michael D. Stemle, Jr <themanchicken@gmail.com>
2013-09-09 02:06:52 +0000

    Fixing the threading.

--- utils/threaded_csv_into_mongo.py
+++ utils/threaded_csv_into_mongo.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
-from __future__ import print_function
-
 import sys, getopt, csv, pprint
 from pymongo import MongoClient
-import thread
+import threading
 import time
-print = lambda x: sys.stdout.write("%s\n" % x)
-
+import traceback
+    
 _THREAD_LIMIT = 10
 
+mongo_lock = threading.Lock()
+print_lock = threading.Lock()
+
 global_mongo = MongoClient(max_pool_size=_THREAD_LIMIT)
-global_threadCount = 0
 
 def usage(msg):
   if msg != None:
     print('\nError: {msg}'.format(msg=msg))
   
   print('\nUsage: {arg0} [options]\n'.format(arg0=sys.argv[0]))
-  print('  Options:\n')
+  print('  Options:\n') 
   print('      -h ......................... Display this help message\n')
   print('      -f|--file=CSVFILE .......... The CSV file to load\n')
   print('      -m|--mongo=MONGOINSTANCE ... The mongo instance to load the file into\n')
@@ -26,46 +26,52 @@ def usage(msg):
 def add_record_to_mongo(mongo, record):
   global global_mongo
   print('CALLED!')
-    
-  mongo_bits = mongo.split('.')
-  if len(mongo_bits) != 2: usage('Mongo Instance format: DBNAME.COLLECTIONNAME')
-  mongo_db = mongo_bits[0]
-  mongo_coll = mongo_bits[1]
   print('Load a record!\n')
   pprint.pprint(record)
   
-#  try:
-#    mydb = global_mongo[mongo_db]
-#    mycoll = mydb[mongo_coll]
+  try:
+    global_mongo.start_request()
+    
+    pprint.pprint(mongo)
+    
+    mydb = global_mongo[mongo['db']]
+    mycoll = mydb[mongo['coll']]
+    
+    global_mongo.end_request()
     
     # Now let's insert
-#    print 'DB: {db} and COLL: {coll}'.format(db=mydb,coll=mycoll)
-#    mycoll.insert(record)
-#  except:
-#    print "Unexpected error:", sys.exc_info()[0], " ", sys.stderr
-  
-#  mycoll.end_request()
-  global_threadCount = global_threadCount - 1
+    print 'DB: {db} and COLL: {coll}'.format(db=mydb,coll=mycoll)
+    # mycoll.insert(record)
+  except:
+    traceback.print_exc()
 
-def process_csv_line(record, mongo):
-  global global_threadCount
+def run_record(incsv, mongo):
+  global mongo_lock
+  global print_lock
   
-  # Wait until we've got a free thread
-  while (global_threadCount >= _THREAD_LIMIT):
-    print('Sleeping while I wait for an open thread...')
-    time.sleep(1)
+  with print_lock: print('Starting thread...')
   
-  print('Starting thread...')
-  global_threadCount = global_threadCount + 1
-  foo = thread.start_new_thread(add_record_to_mongo, (mongo, record, ))
-  foo.run()
+  parsed = csv.DictReader(incsv, delimiter=',', quotechar='"')
+  for record in parsed:
+    with print_lock: add_record_to_mongo(record, mongo)
 
 def run_csv_file(csvfile, mongo):
+  global print_lock
+  
+  tsem = threading.Semaphore(value=_THREAD_LIMIT)
+  all_threads = []
+  
   print('Gonna load the CSV file "{csvfile}" into mongodb "{mongo}"\n'.format(csvfile=csvfile, mongo=mongo))
   with open(csvfile,'rb') as incsv:
-    parsed = csv.DictReader(incsv, delimiter=',', quotechar='"')
-    for record in parsed:
-      process_csv_line(record, mongo)
+    with tsem:
+      with print_lock: print 'Thread is available...'
+      t = threading.Thread(target=run_record, args=(incsv,mongo))
+      t.run()
+      all_threads.append(t)
+  
+  # Join all threads
+  for t in all_threads:
+    if t.isAlive(): t.join()
 
 def main(argv):
   csvfile=None
@@ -86,8 +92,12 @@ def main(argv):
   
   if csvfile == None: usage('Missing CSV file')
   if mongo == None: usage('Missing mongo')
-  
-  run_csv_file(csvfile, mongo)
+
+  mongo_bits = mongo.split('.')
+  if len(mongo_bits) != 2: usage('Mongo Instance format: DBNAME.COLLECTIONNAME')
+  mongo_info = dict(db=mongo_bits[0], coll=mongo_bits[1])
+
+  run_csv_file(csvfile, mongo_info)
 
 if __name__ == '__main__':
   main(sys.argv[1:])
