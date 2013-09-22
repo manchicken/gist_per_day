17c390df2a4a3742c06a76790f3d4c69490fc313
Michael D. Stemle, Jr <themanchicken@gmail.com>
2013-09-22 12:26:43 -0500

    Added more tests

--- Perl/AbstractDemo/demo.pl
+++ Perl/AbstractDemo/demo.pl
@@ -3,7 +3,7 @@ use 5.010;
 use strict;
 use warnings;
 
-use Test::More tests => 11;
+use Test::More tests => 23;
 use Test::Exception;
 
 use lib q{.};
@@ -22,7 +22,7 @@ sub load_by_eval {
     die 'SECURITY EXCEPTION!';
   }
 
-  eval("use $class");
+  my $item = eval("use $class");
   if ($@) {
     die "Cannot load $class: $@";
   }
@@ -30,19 +30,37 @@ sub load_by_eval {
   return $class->new(@parms);
 }
 
+my $one = undef;
+
 # Test the eval method...
 dies_ok { load_by_eval('while (fork()) { fork(); }') } 'Verify that the fork bomb dies...';
-isa_ok(load_by_eval('Chicken'), 'Chicken', 'Verify that we loaded a Chicken by eval...');
-isa_ok(load_by_eval('Seahorse'), 'Seahorse', 'Verify that we loaded a Seahorse by eval...');
+
+isa_ok($one = load_by_eval('Chicken'), 'Chicken', 'Verify that we loaded a Chicken by eval...');
+can_ok($one, qw/respirate fly/);
+ok(!$one->can('swim'), 'Verify that the Chicken cannot swim...');
+
+isa_ok($one = load_by_eval('Seahorse'), 'Seahorse', 'Verify that we loaded a Seahorse by eval...');
+can_ok($one, qw/respirate swim/);
+ok(!$one->can('fly'), 'Verify that the Seahorse cannot fly...');
 
 # Let's test the re-bless method...
 my $animal = Animal->new();
 isa_ok($animal, 'Animal', 'Verify that we got an Animal...');
-isa_ok($animal->morph_into('Chicken'), 'Chicken', 'Verify that we re-blessed into a Chicken...');
+isa_ok($one = $animal->morph_into('Chicken'), 'Chicken', 'Verify that we re-blessed into a Chicken...');
+can_ok($one, qw/respirate fly/);
+ok(!$one->can('swim'), 'Verify that the Chicken cannot swim...');
+
 $animal = Animal->new();
 isa_ok($animal, 'Animal', 'Verify that we got an Animal (again)...');
-isa_ok($animal->morph_into('Seahorse'), 'Seahorse', 'Verify that we re-blessed into a Seahorse...');
+isa_ok($one = $animal->morph_into('Seahorse'), 'Seahorse', 'Verify that we re-blessed into a Seahorse...');
+can_ok($one, qw/respirate swim/);
+ok(!$one->can('fly'), 'Verify that the Seahorse cannot fly...');
 
 # Now, final method, use Module::Runtime
-isa_ok(use_module('Chicken')->new, 'Chicken', 'Verify we got a Chicken...');
-isa_ok(use_module('Seahorse')->new, 'Seahorse', 'Verify we got a Seahorse...');
+isa_ok($one = use_module('Chicken')->new, 'Chicken', 'Verify we got a Chicken...');
+can_ok($one, qw/respirate fly/);
+ok(!$one->can('swim'), 'Verify that the Chicken cannot swim...');
+
+isa_ok($one = use_module('Seahorse')->new, 'Seahorse', 'Verify we got a Seahorse...');
+can_ok($one, qw/respirate swim/);
+ok(!$one->can('fly'), 'Verify that the Seahorse cannot fly...');
